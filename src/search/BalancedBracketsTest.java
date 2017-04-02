06e24ad4c8c246af9d0a53d8eb0d51ce1d5dc51f
baran ipek <baranipek@barans-MacBook-Pro.local>
2017-04-02 13:59:17 +0300

    Balanced brackets

---
+++ src/search/BalancedBracketsTest.java
@@ -0,0 +1,26 @@
+package search;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by baranipek on 02/04/17.
+ */
+public class BalancedBracketsTest extends TestCase {
+
+
+    @Test
+    public void testIsBalanced() throws Exception {
+
+        assertTrue(BalancedBrackets.isBalanced("(){}"));
+
+    }
+
+    @Test
+    public void testIsNotBalanced() throws Exception {
+
+        assertFalse(BalancedBrackets.isBalanced("(){}["));
+
+    }
+
+}
