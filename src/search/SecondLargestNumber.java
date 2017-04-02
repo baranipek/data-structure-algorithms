4dd88ae42103b8d0a1d55a189be598828bd856fc
baran ipek <baranipek@barans-MacBook-Pro.local>
2017-04-02 16:13:13 +0300

    SecondLargestElementInList

---
+++ src/search/SecondLargestNumber.java
@@ -0,0 +1,29 @@
+package search;
+
+import java.util.List;
+
+
+public class SecondLargestNumber {
+
+    static Integer findSecondLargestNumber(List<Integer> list) {
+
+        int largestNumber = 0;
+        int secondLargestNumber = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            int num = list.get(i);
+            if (num > largestNumber) {
+                secondLargestNumber = largestNumber;
+                largestNumber = num;
+            } else if (num > secondLargestNumber) {
+                secondLargestNumber = num;
+            }
+
+        }
+
+
+        return secondLargestNumber;
+
+    }
+
+}
---
+++ src/search/SecondLargestNumberTest.java
@@ -0,0 +1,24 @@
+package search;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by baranipek on 02/04/17.
+ */
+public class SecondLargestNumberTest extends TestCase {
+    public void testFindSecondLargestNumber() throws Exception {
+        List<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(5);
+        list.add(6);
+        list.add(2);
+
+        assertEquals(SecondLargestNumber.findSecondLargestNumber(list).toString(),"5");
+
+    }
+
+}
